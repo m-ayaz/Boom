8197513a58e50a3382971767fd8e958bca685021
m-ayaz <62539686+m-ayaz@users.noreply.github.com>
2024-01-02 06:32:10 +0330

    Update Test63.java

--- src/main/java/com/boom/test/Test63.java
+++ src/main/java/com/boom/test/Test63.java
@@ -2,13 +2,22 @@ package com.boom.test;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.QuadCurve;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.converter.FormatStringConverter;
+
+import java.text.MessageFormat;
 
 import static com.boom.tools.Tools.*;
 
@@ -26,8 +35,76 @@ public class Test63 extends Application {
         Scene scene = new Scene(container);
         stage.setScene(scene);
         stage.show();
+        Circle circle=new Circle(5,Color.BLUE);
 
-        
+
+
+        String string="Some Text";
+
+        Text text=new Text(string);
+        TextArea textArea=new TextArea(string);
+
+        Rectangle rectangle1=new Rectangle();
+        Rectangle rectangle2=new Rectangle();
+
+        text.setFont(new Font(50));
+
+        container.getChildren().add(textArea);
+//        container.getChildren().add(text);
+        container.getChildren().addAll(rectangle1,rectangle2,circle);
+        text.setTranslateX(100);
+        text.setTranslateY(100);
+        text.setCaretBias(false);
+//        text.set
+
+        textArea.setFont(new Font(100));
+
+        text.setBoundsType(TextBoundsType.VISUAL);
+
+//        textArea.setStyle("-fx-label-padding: 0");
+
+//        textArea.setStyle("  -fx-padding: -5px;\n" +
+//                "    -fx-border-insets: -5px;\n" +
+//                "    -fx-background-insets: -5px;");
+//        TextFormatter<Rectangle> h= new TextFormatter<>(new Rectangle());
+
+//        h.
+
+//        textArea.setTextFormatter();
+
+//        textArea.setScrollTop(0);
+//        textArea.setScrollLeft(0);
+
+
+//        textArea.setScrollTop(1000);
+//        textArea.setBo
+
+
+
+
+
+
+
+
+
+
+        rectangle1.setFill(new Color(1,0,0,0.1));
+
+        rectangle1.setX(textArea.getBoundsInParent().getMinX());
+        rectangle1.setY(textArea.getBoundsInParent().getMinY());
+        rectangle1.setWidth(textArea.getBoundsInParent().getWidth());
+        rectangle1.setHeight(textArea.getBoundsInParent().getHeight());
+        circle.setCenterX(rectangle1.getBoundsInParent().getMinX());
+        circle.setCenterY(rectangle1.getBoundsInParent().getMaxY());
+
+        textArea.boundsInParentProperty().addListener((a,b,c)->{
+            print(c);
+            rectangle1.setX(c.getMinX());
+            rectangle1.setY(c.getMinY());
+            rectangle1.setWidth(c.getWidth());
+            rectangle1.setHeight(c.getHeight());
+
+        });
 
     }
 
